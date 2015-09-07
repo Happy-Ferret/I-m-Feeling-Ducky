1920c239c2be0087bd758d199e03d13b16c11d01
Noitidart <noitidart@gmail.com>
2015-09-06 21:36:09 -0700

    1.0-night.initial
    Set up basic localized bootstrap structure.
    Using temporary yucky DDG icon for now

---
+++ bootstrap.js
@@ -0,0 +1,116 @@
+// Imports
+const {classes: Cc, interfaces: Ci, manager: Cm, results: Cr, utils: Cu, Constructor: CC} = Components;
+
+Cu.import('resource://gre/modules/devtools/Console.jsm');
+Cu.import('resource://gre/modules/Services.jsm');
+Cu.import('resource://gre/modules/XPCOMUtils.jsm');
+
+// Globals
+const core = {
+	addon: {
+		name: 'I\'m Feeling Ducky',
+		id: 'ImFeelingDucky@jetpack',
+		path: {
+			name: 'imfeelingducky',
+			content: 'chrome://imfeelingducky/content/',
+			images: 'chrome://imfeelingducky/content/resources/images/',
+			locale: 'chrome://imfeelingducky/locale/',
+			resources: 'chrome://imfeelingducky/content/resources/',
+		},
+		cache_key: Math.random() // set to version on release
+	},
+	os: {
+		name: OS.Constants.Sys.Name.toLowerCase(),
+		toolkit: Services.appinfo.widgetToolkit.toLowerCase(),
+		xpcomabi: Services.appinfo.XPCOMABI
+	},
+	firefox: {
+		pid: Services.appinfo.processID,
+		version: Services.appinfo.version
+	}
+};
+
+const JETPACK_DIR_BASENAME = 'jetpack';
+const myPrefBranch = 'extensions.' + core.addon.id + '.';
+
+// Lazy Imports
+const myServices = {};
+XPCOMUtils.defineLazyGetter(myServices, 'hph', function () { return Cc['@mozilla.org/network/protocol;1?name=http'].getService(Ci.nsIHttpProtocolHandler); });
+XPCOMUtils.defineLazyGetter(myServices, 'sb', function () { return Services.strings.createBundle(core.addon.path.locale + 'bootstrap.properties?' + core.addon.cache_key); /* Randomize URI to work around bug 719376 */ });
+
+function extendCore() {
+	// adds some properties i use to core based on the current operating system, it needs a switch, thats why i couldnt put it into the core obj at top
+	switch (core.os.name) {
+		case 'winnt':
+		case 'winmo':
+		case 'wince':
+			core.os.version = parseFloat(Services.sysinfo.getProperty('version'));
+			// http://en.wikipedia.org/wiki/List_of_Microsoft_Windows_versions
+			if (core.os.version == 6.0) {
+				core.os.version_name = 'vista';
+			}
+			if (core.os.version >= 6.1) {
+				core.os.version_name = '7+';
+			}
+			if (core.os.version == 5.1 || core.os.version == 5.2) { // 5.2 is 64bit xp
+				core.os.version_name = 'xp';
+			}
+			break;
+			
+		case 'darwin':
+			var userAgent = myServices.hph.userAgent;
+
+			var version_osx = userAgent.match(/Mac OS X 10\.([\d\.]+)/);
+
+			
+			if (!version_osx) {
+				throw new Error('Could not identify Mac OS X version.');
+			} else {
+				var version_osx_str = version_osx[1];
+				var ints_split = version_osx[1].split('.');
+				if (ints_split.length == 1) {
+					core.os.version = parseInt(ints_split[0]);
+				} else if (ints_split.length >= 2) {
+					core.os.version = ints_split[0] + '.' + ints_split[1];
+					if (ints_split.length > 2) {
+						core.os.version += ints_split.slice(2).join('');
+					}
+					core.os.version = parseFloat(core.os.version);
+				}
+				// this makes it so that 10.10.0 becomes 10.100
+				// 10.10.1 => 10.101
+				// so can compare numerically, as 10.100 is less then 10.101
+				
+				//core.os.version = 6.9; // note: debug: temporarily forcing mac to be 10.6 so we can test kqueue
+			}
+			break;
+		default:
+			// nothing special
+	}
+	
+
+}
+
+// START - Addon Functionalities					
+
+// END - Addon Functionalities
+
+
+function install() {}
+
+function uninstall(aData, aReason) {}
+
+function startup(aData, aReason) {
+	// core.addon.aData = aData;
+	extendCore();
+	
+}
+
+function shutdown(aData, aReason) {
+	if (aReason == APP_SHUTDOWN) { return }
+
+}
+
+// start - common helper functions
+
+// end - common helper functions
